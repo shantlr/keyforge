5eb0ff1878b577fd56690a8fac888e18762c3830
Patrick <shantry@hotmail.fr>
2023-07-08 12:42:41 +0200

    Add more parser c tests

---
+++ parse/src/test/parser/ast/expression/gte.test.ts
@@ -0,0 +1,21 @@
+import { parseC } from '../../../../parser';
+
+describe('parser', () => {
+  describe('value expression', () => {
+    it('should parse a >= b', () => {
+      const ast = parseC(`test >= 1`, (p) => p.valueExpression());
+      expect(ast).toEqual({
+        type: 'gte',
+        values: ['test', 1],
+      });
+    });
+
+    it('should parse a >= b >= c', () => {
+      const ast = parseC(`test >= 1 >= 2`, (p) => p.valueExpression());
+      expect(ast).toEqual({
+        type: 'gte',
+        values: ['test', 1, 2],
+      });
+    });
+  });
+});
