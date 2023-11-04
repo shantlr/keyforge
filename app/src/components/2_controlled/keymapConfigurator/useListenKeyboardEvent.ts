1437503eeba5810f45d280e39dbaddb13926cf9d
Patrick <shantry@hotmail.fr>
2023-11-04 17:36:54 +0100

    app: improve register shift key

--- app/src/components/2_controlled/keymapConfigurator/useListenKeyboardEvent.ts
+++ app/src/components/2_controlled/keymapConfigurator/useListenKeyboardEvent.ts
@@ -1,58 +1,119 @@
+import { forEach } from 'lodash';
 import { useEffect, useRef } from 'react';
 
-const toKeycode = (e: KeyboardEvent) => {
+import { KeyEnum } from '@/constants';
+
+const ControlKeys: Record<
+  string,
+  {
+    key: 'ctrl' | 'alt' | 'gui' | 'shift';
+    skip:
+      | 'shouldSkipCtrlRelease'
+      | 'shouldSkipShiftRelease'
+      | 'shouldSkipAltRelease'
+      | 'shouldSkipGuiRelease';
+  }
+> = {
+  Control: {
+    key: 'ctrl',
+    skip: 'shouldSkipCtrlRelease',
+  },
+  Shift: {
+    key: 'shift',
+    skip: 'shouldSkipShiftRelease',
+  },
+  Alt: {
+    key: 'alt',
+    skip: 'shouldSkipAltRelease',
+  },
+  Meta: {
+    key: 'gui',
+    skip: 'shouldSkipGuiRelease',
+  },
+};
+
+const toKeycode = (
+  e: KeyboardEvent,
+  state: { shift?: boolean }
+): KeyEnum | null => {
   switch (e.key) {
     case 'a':
+    case 'A':
       return 'KC_A';
     case 'b':
+    case 'B':
       return 'KC_B';
     case 'c':
+    case 'C':
       return 'KC_C';
     case 'd':
+    case 'D':
       return 'KC_D';
     case 'e':
+    case 'E':
       return 'KC_E';
     case 'f':
+    case 'F':
       return 'KC_F';
     case 'g':
+    case 'G':
       return 'KC_G';
     case 'h':
+    case 'H':
       return 'KC_H';
     case 'i':
+    case 'I':
       return 'KC_I';
     case 'j':
+    case 'J':
       return 'KC_J';
     case 'k':
+    case 'K':
       return 'KC_K';
     case 'l':
+    case 'L':
       return 'KC_L';
     case 'm':
+    case 'M':
       return 'KC_M';
     case 'n':
+    case 'N':
       return 'KC_N';
     case 'o':
+    case 'O':
       return 'KC_O';
     case 'p':
+    case 'P':
       return 'KC_P';
     case 'q':
+    case 'Q':
       return 'KC_Q';
     case 'r':
+    case 'R':
       return 'KC_R';
     case 's':
+    case 'S':
       return 'KC_S';
     case 't':
+    case 'T':
       return 'KC_T';
     case 'u':
+    case 'U':
       return 'KC_U';
     case 'v':
+    case 'V':
       return 'KC_V';
     case 'w':
+    case 'W':
       return 'KC_W';
     case 'x':
+    case 'X':
       return 'KC_X';
     case 'y':
+    case 'Y':
       return 'KC_Y';
     case 'z':
+    case 'Z':
       return 'KC_Z';
     case 'Escape':
       return 'KC_ESC';
@@ -111,26 +172,66 @@ const toKeycode = (e: KeyboardEvent) => {
   }
 
   switch (e.code) {
-    case 'Digit1':
+    case 'Digit1': {
+      if (state.shift) {
+        return 'KC_EXLM';
+      }
       return 'KC_1';
-    case 'Digit2':
+    }
+    case 'Digit2': {
+      if (state.shift) {
+        return 'KC_AT';
+      }
       return 'KC_2';
-    case 'Digit3':
+    }
+    case 'Digit3': {
+      if (state.shift) {
+        return 'KC_HASH';
+      }
       return 'KC_3';
-    case 'Digit4':
+    }
+    case 'Digit4': {
+      if (state.shift) {
+        return 'KC_DLR';
+      }
       return 'KC_4';
-    case 'Digit5':
+    }
+    case 'Digit5': {
+      if (state.shift) {
+        return 'KC_PERC';
+      }
       return 'KC_5';
-    case 'Digit6':
+    }
+    case 'Digit6': {
+      if (state.shift) {
+        return 'KC_CIRC';
+      }
       return 'KC_6';
-    case 'Digit7':
+    }
+    case 'Digit7': {
+      if (state.shift) {
+        return 'KC_AMPR';
+      }
       return 'KC_7';
-    case 'Digit8':
+    }
+    case 'Digit8': {
+      if (state.shift) {
+        return 'KC_ASTR';
+      }
       return 'KC_8';
-    case 'Digit9':
+    }
+    case 'Digit9': {
+      if (state.shift) {
+        return 'KC_LPRN';
+      }
       return 'KC_9';
-    case 'Digit0':
+    }
+    case 'Digit0': {
+      if (state.shift) {
+        return 'KC_RPRN';
+      }
       return 'KC_0';
+    }
     case 'Backquote':
       return 'KC_GRV';
     case 'Backslash':
@@ -146,6 +247,8 @@ const toKeycode = (e: KeyboardEvent) => {
     case 'Minus':
       return 'KC_MINS';
   }
+
+  return null;
 };
 
 export const useListenKeyboardEvent = (
@@ -155,24 +258,67 @@ export const useListenKeyboardEvent = (
   const ref = useRef<typeof cb>();
   ref.current = cb;
 
+  const stateRef = useRef<{
+    shift?: boolean;
+    shouldSkipShiftRelease?: boolean;
+
+    ctrl?: boolean;
+    shouldSkipCtrlRelease?: boolean;
+
+    gui?: boolean;
+    shouldSkipGuiRelease?: boolean;
+
+    alt?: boolean;
+    shouldSkipAltRelease?: boolean;
+  }>({});
+
   useEffect(() => {
     if (!enabled) {
       return;
     }
 
-    const listener = (e: KeyboardEvent) => {
-      const kc = toKeycode(e);
-      if (kc) {
-        e.preventDefault();
-        e.stopPropagation();
-        ref.current?.(kc);
-      } else {
-        console.log(e);
+    const downListener = (e: KeyboardEvent) => {
+      if (e.key in ControlKeys) {
+        stateRef.current[ControlKeys[e.key].key] = true;
       }
     };
+
+    const listener = (e: KeyboardEvent) => {
+      try {
+        if (e.key in ControlKeys && stateRef.current[ControlKeys[e.key].skip]) {
+          e.preventDefault();
+          e.stopPropagation();
+          return;
+        }
+
+        const kc = toKeycode(e, stateRef.current);
+        forEach(ControlKeys, ({ key, skip }, eKey) => {
+          if (stateRef.current[key] && e.key !== eKey) {
+            stateRef.current[skip] = true;
+          }
+        });
+
+        if (kc) {
+          e.preventDefault();
+          e.stopPropagation();
+          ref.current?.(kc);
+        } else {
+          console.log(e);
+        }
+      } finally {
+        if (e.key in ControlKeys) {
+          const k = ControlKeys[e.key];
+          stateRef.current[k.skip] = false;
+          stateRef.current[k.key] = false;
+        }
+      }
+    };
+
     window.addEventListener('keyup', listener);
+    window.addEventListener('keydown', downListener);
     return () => {
       window.removeEventListener('keyup', listener);
+      window.removeEventListener('keydown', downListener);
     };
   }, [enabled]);
 };
